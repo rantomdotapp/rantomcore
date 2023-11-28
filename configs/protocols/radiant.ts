2e1316271167de70655ae45259d641eae67ec760
Noat Eden <noat.eth@gmail.com>
2023-11-28 22:45:47 +0700

    add radiant ethereum market

--- configs/protocols/radiant.ts
+++ configs/protocols/radiant.ts
@@ -4,6 +4,11 @@ export const RadiantConfigs: ProtocolConfig = {
   protocol: 'radiant',
   contracts: [
     {
+      chain: 'ethereum',
+      protocol: 'radiant',
+      address: '0xa950974f64aa33f27f6c5e017eee93bf7588ed07',
+    },
+    {
       chain: 'arbitrum',
       protocol: 'radiant',
       address: '0xf4b1486dd74d07706052a33d31d7c0aafd0659e1',
